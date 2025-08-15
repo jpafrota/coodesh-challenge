354613aee850c682f075b52661917ce2873a12a5
Jorge Frota <jorgepabloafrota@gmail.com>
2025-08-15 11:52:31 -0400

    style: format files
    Signed-off-by: Jorge Frota <jorgepabloafrota@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { UsersModule } from "./users/users.module";
+import { UsersController } from "src/users/users.controller";
+import { UsersService } from "src/users/users.service";
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
