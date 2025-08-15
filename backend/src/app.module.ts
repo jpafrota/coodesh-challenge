ee88706862e9b2140c4b1f8cf1f66bc9eea5e923
Jorge Frota <jorgepabloafrota@gmail.com>
2025-08-15 13:47:10 -0400

    feat: create auth and root resources
    Signed-off-by: Jorge Frota <jorgepabloafrota@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UsersModule } from "./users/users.module";
 import { WordsModule } from "./words/words.module";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, WordsModule],
+  imports: [UsersModule, WordsModule, AuthModule],
   controllers: [],
   providers: [],
 })
